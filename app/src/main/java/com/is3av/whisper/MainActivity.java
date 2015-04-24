182844a93c256912b3c3301a0c65dfd7eb05fbf3
Sai Ongole <svo214@nyu.edu>
2015-04-24 15:16:17 -0400

    Activity working. Write a loop for naming the files.

--- app/src/main/java/com/is3av/whisper/MainActivity.java
+++ app/src/main/java/com/is3av/whisper/MainActivity.java
@@ -1,39 +1,70 @@
 package com.is3av.whisper;
 
-import android.support.v7.app.ActionBarActivity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity  {
+    private Button button;
+    private TextView tv;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setText("Start");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(button.getText().equals("Start")) {
+                    button.setText("Stop");
+                    // Start recording here
+                    try {
+                        myAudioRecorder.prepare();
+                        myAudioRecorder.start();
+                    } catch (IllegalStateException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    button.setText("Start");
+                    // save the file here
+                    myAudioRecorder.stop();
+                    myAudioRecorder.release();
+                    myAudioRecorder  = null;
+                    Toast.makeText(getApplicationContext(), "Audio recorded successfully",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        outputFile = (Environment.getExternalStorageDirectory().getAbsolutePath()).toString() + File.separator + "recording.mp4";
+
+        myAudioRecorder = new MediaRecorder();
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    public void stop(View view){
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
